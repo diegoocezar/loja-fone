677d5eb88943af623eddd597bb195a0f6e7878ed
Diego Cezar de Oliveira Bernardino <diego.cezar@hemorio.net>
2019-04-01 12:37:10 -0300

    Desafio avançado aula 013 - v1

--- js/product-description.js
+++ js/product-description.js
@@ -8,13 +8,14 @@
 //Desafio básico: pegar o evento de click no botão "Adiciona no carrinho" e exibir no console
 //Desafio intermediário: Exibir uma mensagem ao clicar no botão com o texto "ae"
 //No desafio intermediário optei por exibir tanto no console quanto no alert.
+//Desafio avançado: Modificar o texto da Nav onde está escrito "Carrinho(10)" ao clicar no botão "Adiciona no carrinho".
 const $cart = document.querySelector(".-second");
+const $navigationCart = document.querySelector(".-last");
 
 $cart.addEventListener("click", handleClick);
 
 function handleClick(){
-    console.log ("ae");
-    alert("ae");
+    $navigationCart.textContent = "Carrinho (11)";
 }
 
 
