704aad49a0ee113da6955f20294b85dacf87023a
Diego Cezar de Oliveira Bernardino <diego.cezar@hemorio.net>
2019-04-01 12:26:15 -0300

    Desafio básico aula 013 - v1

--- js/product-description.js
+++ js/product-description.js
@@ -1,7 +1,15 @@
-var heart = document.querySelector(".heart");
+// const heart = document.querySelector(".heart");
 
-heart.addEventListener("click", function(){
-    heart.classList.add("-heart");
-})
+// heart.addEventListener("click", function(){
+//     heart.classList.add("-heart");
+// })
 
 
+//Desafio básico: pegar o evento de click no botão "Adiciona no carrinho" e exibir no console
+const $cart = document.querySelector(".-second");
+
+$cart.addEventListener("click", handleClick);
+
+function handleClick(){
+    console.log ($cart);
+}
