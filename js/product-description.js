614b07b3853e07250de752264273fdb790fe7cc9
Diego Cezar de Oliveira Bernardino <diego.cezar@hemorio.net>
2019-04-01 12:28:50 -0300

    Desafio intermediário aula 013 - v1

--- js/product-description.js
+++ js/product-description.js
@@ -6,10 +6,15 @@
 
 
 //Desafio básico: pegar o evento de click no botão "Adiciona no carrinho" e exibir no console
+//Desafio intermediário: Exibir uma mensagem ao clicar no botão com o texto "ae"
+//No desafio intermediário optei por exibir tanto no console quanto no alert.
 const $cart = document.querySelector(".-second");
 
 $cart.addEventListener("click", handleClick);
 
 function handleClick(){
-    console.log ($cart);
+    console.log ("ae");
+    alert("ae");
 }
+
+
