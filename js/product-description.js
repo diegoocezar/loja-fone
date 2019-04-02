39f36cec7d4ce7adc88a3c6ae228caa22c9dce1e
Diego Cezar <diegocezar.comercial@gmail.com>
2019-04-02 03:24:27 +0000

    Refatorando e corrigindo código na aula 016

--- js/product-description.js
+++ js/product-description.js
@@ -1,20 +1,67 @@
+/* 
+Com o document.querySelectorAll() somos capazes de obter todos os elementos que possuem determinada classe/ID/tag e nos é retornado uma lista com todos esses elementos;
+Podemos percorrer essa lista com o comando .forEach e executar uma função para cada vez que formos avançando na lista;
+Utilizando a palavra reservada "this" que tem valor dinâmico, podemos atribuir o valor que a variável possui naquele momento e posição da lista;
+*/
 const $heart = document.querySelector(".-heart");
+const $stars = document.querySelectorAll(".star");
+const $lastPosition = $stars.length - 1;
 
-const $firstStar = document.querySelectorAll(".star");
+$heart.addEventListener("click", handleClick)
 
-$heart.addEventListener("click", function(){
-    $heart.classList.toggle("-active");
+/* Percorre a lista e executa uma função para testar qual estrela foi clicada, dependedo da posição da estrela na lista, é executada uma função específica que fará a adição ou remoção da classe "-active"*/
+$stars.forEach(function($star, key){
+    if (key == 0){
+        $star.addEventListener("click", firstStar);
+    }
+
+    if (key == $lastPosition) {
+        $star.addEventListener("click", lastStar);
+    }
+
+    if (key > 0 && key < $lastPosition){
+        $star.addEventListener("click", function(){
+            middleStar(key);
+        })
+    }
 })
 
+function handleClick(){
+    this.classList.toggle("-active");
+}
 
-console.log($firstStar[1]);
-
-for (var i=0; i<5; i++){
-    console.log (i);
-    $firstStar[i].addEventListener("click", function(){
-        $firstStar[i].classList.toggle("-active");
+/* Percorre a lista removendo a classe "-active" de todos os itens;
+   Adiciona a classe "-active" no elemento clicado - this
+ */
+function firstStar(){
+    $stars.forEach(function($star){
+        $star.classList.remove("-active");
     });
+
+    this.classList.add("-active");
     
+}
+
+/* Essa função recebe como parâmetro o index que é passado no momento da chamada, esse índice é a posição do elemento clicado na lista;
+    Percorre a lista removendo a classe "-active" de todos os itens;
+    Testa se o valor do objeto na lista é menor ou igual ao índice informado anteriormente, se for, adiciona a classe "-active" nos objetos anteriores e no objeto clicado.
+*/
+function middleStar(index){
+    $stars.forEach(function($star, key){
+        $star.classList.remove("-active");
+
+        if(key <= index){
+            $star.classList.add("-active");
+        }
+    });
+}
+
+/* Percorre a lista e adiciona a classe "-active" em todos os objetos.
+ */
+function lastStar(){
+    $stars.forEach(function($star){
+        $star.classList.add("-active");
+    });
 }
 
 
