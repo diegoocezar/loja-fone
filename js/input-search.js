3f00514b63405a23cd82a087d17ce71deaac3f69
Diego Cezar <diegocezar.comercial@gmail.com>
2019-04-04 03:48:31 +0000

    Fim da animação do search e refatoração de código

--- js/input-search.js
+++ js/input-search.js
@@ -1,7 +1,21 @@
 const $inputSearch = document.querySelector(".input-search");
-const $icon = document.querySelector(".icon");
+const $magnifier = document.querySelector(".magnifier");
+const $close = document.querySelector(".close");
 
-$icon.addEventListener("click", () => {
+$magnifier.addEventListener("click", () => {
     event.preventDefault();
+    
     $inputSearch.classList.toggle("-active");
+    
+    $magnifier.classList.remove("-active");
+    $close.classList.add("-active");
+})
+
+$close.addEventListener("click", () => {
+    event.preventDefault();
+    
+    $inputSearch.classList.toggle("-active");
+    
+    $close.classList.remove("-active");
+    $magnifier.classList.add("-active");
 })
