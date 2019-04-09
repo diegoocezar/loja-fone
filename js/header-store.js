eb9fccaf9ad54269f0e33080c382346a5631b78d
diegoocezar <diegocezar.comercial@gmail.com>
2019-04-09 00:16:39 -0300

    Efeito no menu  hamburguer

---
+++ js/header-store.js
@@ -0,0 +1,8 @@
+const list = document.querySelector(".list");
+const menu = document.querySelector(".-menu");
+
+menu.addEventListener("click", ()=>{
+    list.classList.toggle("-active");
+    
+
+})
